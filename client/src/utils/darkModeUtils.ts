facf4c691d1c892d7fa856bad3e2f56dabb72c6e
Brink5459 <82228865+Brink5459@users.noreply.github.com>
2026-04-12 03:23:14 +0800

    Update darkModeUtils.ts

--- client/src/utils/darkModeUtils.ts
+++ client/src/utils/darkModeUtils.ts
@@ -1,24 +1,8 @@
 import { useState, useEffect } from "react";
 
 export function listenSystemMode() {
-  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-  function darkModeHandler() {
-    const mode = localStorage.getItem("theme");
-    if (mode === null || mode === "system") {
-      if (mediaQuery.matches) {
-        document.documentElement.setAttribute("data-color-mode", "dark");
-      } else {
-        document.documentElement.setAttribute("data-color-mode", "light");
-      }
-      window.dispatchEvent(new Event("colorSchemeChange"));
-    }
-  }
-
-  // 判断当前模式
-  darkModeHandler();
-  // 监听模式变化
-  mediaQuery.addEventListener("change", darkModeHandler);
+  document.documentElement.setAttribute("data-color-mode", "light");
+  localStorage.setItem("theme", "light");
 }
 
 export function getCurrentColorMode(): "light" | "dark" {
